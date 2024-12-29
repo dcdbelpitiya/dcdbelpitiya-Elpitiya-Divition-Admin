600714a969909a579098c8ce4321ea34348c0c91
dcdbelpitiya <dcdb.elpitiya2@gmail.com>
2024-12-29 21:26:14 +0530

    Add files via upload

---
+++ mainheader.js
@@ -0,0 +1,13 @@
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK-v5MPfbYO5dyyUOuBqJqSMX8CUwyprc",
+  authDomain: "elpitiyadivision-701e8.firebaseapp.com",
+  databaseURL: "https://elpitiyadivision-701e8-default-rtdb.firebaseio.com",
+  projectId: "elpitiyadivision-701e8",
+  storageBucket: "elpitiyadivision-701e8.appspot.com",
+  messagingSenderId: "845789496115",
+  appId: "1:845789496115:web:4dbc0d8263533543947446",
+  measurementId: "G-EHJXVS604T"
+};
+
